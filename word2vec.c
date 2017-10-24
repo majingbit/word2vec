42186b42f2ff4c5b61ef499ef824163695c240d9
majing <majingbit@163.com>
2017-10-24 17:37:09 +0800

    majing modify

--- word2vec.c
+++ word2vec.c
@@ -580,6 +580,7 @@ void *TrainModelThread(void *id) {
         for (c = 0; c < layer1_size; c++) syn0[c + l1] += neu1e[c];
       }
     }
+    
     sentence_position++;
     if (sentence_position >= sentence_length) {
       sentence_length = 0;
@@ -610,41 +611,45 @@ void TrainModel() {
   start = clock();
   for (a = 0; a < num_threads; a++) pthread_create(&pt[a], NULL, TrainModelThread, (void *)a);
   for (a = 0; a < num_threads; a++) pthread_join(pt[a], NULL);
+  //写入到文件
   fo = fopen(output_file, "wb");
   if (classes == 0) {
     // Save the word vectors
     fprintf(fo, "%lld %lld\n", vocab_size, layer1_size);
     for (a = 0; a < vocab_size; a++) {
       fprintf(fo, "%s ", vocab[a].word);
+      //二进制格式
       if (binary) for (b = 0; b < layer1_size; b++) fwrite(&syn0[a * layer1_size + b], sizeof(real), 1, fo);
+      //一般的格式
       else for (b = 0; b < layer1_size; b++) fprintf(fo, "%lf ", syn0[a * layer1_size + b]);
       fprintf(fo, "\n");
     }
   } else {
     // Run K-means on the word vectors
+    //classes为聚类个数
     int clcn = classes, iter = 10, closeid;
-    int *centcn = (int *)malloc(classes * sizeof(int));
-    int *cl = (int *)calloc(vocab_size, sizeof(int));
+    int *centcn = (int *)malloc(classes * sizeof(int)); //该类别的数量
+    int *cl = (int *)calloc(vocab_size, sizeof(int));   //词到类别的映射
     real closev, x;
-    real *cent = (real *)calloc(classes * layer1_size, sizeof(real));
+    real *cent = (real *)calloc(classes * layer1_size, sizeof(real)); //质心数组
     for (a = 0; a < vocab_size; a++) cl[a] = a % clcn;
     for (a = 0; a < iter; a++) {
-      for (b = 0; b < clcn * layer1_size; b++) cent[b] = 0;
+      for (b = 0; b < clcn * layer1_size; b++) cent[b] = 0; //质心清零
       for (b = 0; b < clcn; b++) centcn[b] = 1;
       for (c = 0; c < vocab_size; c++) {
-        for (d = 0; d < layer1_size; d++) cent[layer1_size * cl[c] + d] += syn0[c * layer1_size + d];
-        centcn[cl[c]]++;
+        for (d = 0; d < layer1_size; d++) cent[layer1_size * cl[c] + d] += syn0[c * layer1_size + d]; //求和放到质心数组中
+        centcn[cl[c]]++; //类别数量加1 
       }
-      for (b = 0; b < clcn; b++) {
+      for (b = 0; b < clcn; b++) { //遍历所有类别 
         closev = 0;
         for (c = 0; c < layer1_size; c++) {
-          cent[layer1_size * b + c] /= centcn[b];
+          cent[layer1_size * b + c] /= centcn[b]; //均值，就是求新的质心
           closev += cent[layer1_size * b + c] * cent[layer1_size * b + c];
         }
         closev = sqrt(closev);
-        for (c = 0; c < layer1_size; c++) cent[layer1_size * b + c] /= closev;
+        for (c = 0; c < layer1_size; c++) cent[layer1_size * b + c] /= closev; //对质心进行归一化
       }
-      for (c = 0; c < vocab_size; c++) {
+      for (c = 0; c < vocab_size; c++) { //对所有词语重新分类  
         closev = -10;
         closeid = 0;
         for (d = 0; d < clcn; d++) {
